715681ef0b0998fb5a1298f393d73192a0565937
mukesh-ridgetech <mukesh@ridgetechcorp.com>
2025-01-06 11:56:38 +0530

    backend url is change for frontend

--- src/app/helper/Helper.js
+++ src/app/helper/Helper.js
@@ -1,5 +1,5 @@
 // const baseurl = "http://localhost:5000";
 
-const baseurl = "https://ajitbackend1.onrender.com";
-// const baseurl = "https://api.diwan-realty.com";
+// const baseurl = "https://ajitbackend1.onrender.com";
+const baseurl = "https://api.leadershipladder.in";
 export { baseurl };
